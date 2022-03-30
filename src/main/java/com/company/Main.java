acecf842fa4738f36cfe7c7d50bd5e502548ed5b
hoang nguyen huu <hoang.nguyenhuu@lotussoft.vn>
2022-03-31 05:58:03 +0700

    Add log4j logging

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,9 +1,16 @@
 package com.company;
 
-public class Main {
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
+public class Main {
+    static  Logger logger = LogManager.getLogger(Main.class);
     public static void main(String[] args) {
 	// write your code here
+        BasicConfigurator.configure();
+        logger.debug("Đây là câu lệnh debug");
+        logger.info("Đây là câu lệnh info");
 
     }
 }
